a17b7a41f866a9744a0b1f2d6235ca48eb61a6c2
xSnapi <63411420+xSnapi@users.noreply.github.com>
2022-01-31 03:56:33 +0100

    Added files

--- Heavy OpenGL/include/Heavy OpenGL.hpp
+++ Heavy OpenGL/include/Heavy OpenGL.hpp
@@ -2,5 +2,24 @@
 
 #include "Window.hpp"
 
+#include "Core.hpp"
+#include "Heavy Times.hpp"
+
+#include "Camera.hpp"
+
+#include "Mouse.hpp"
+#include "Keyboard.hpp"
+
+#include "Texture.hpp"
+#include "Vertex.hpp"
+#include "Shader.hpp"
+#include "Vertex Array.hpp"
+#include "Clock.hpp"
+
+#include "Rectangle Shape.hpp"
+#include "Circle Shape.hpp"
+
 #include <GLEW/glew.h>
 #include <GLFW/glfw3.h>
+#include <GLM/glm.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
