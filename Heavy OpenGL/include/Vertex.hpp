2e842f767d024b9ba442e3b5c847cbb8d1d0ef08
xSnapi <63411420+xSnapi@users.noreply.github.com>
2022-01-31 03:58:29 +0100

    Added constructors and 3D Vertex

--- Heavy OpenGL/include/Vertex.hpp
+++ Heavy OpenGL/include/Vertex.hpp
@@ -5,7 +5,73 @@
 namespace hv {
 	struct Vertex2D
 	{
-		glm::vec2 Position;
-		glm::vec2 TexCoord;
+		glm::vec2 Position = glm::vec2(0.0f);
+		glm::vec2 TexCoord = glm::vec2(0.0f);
+		glm::vec4 Color    = glm::vec4(1.0f);
+
+		Vertex2D(glm::vec2 position)
+			: Position(position)
+		{
+
+		}
+
+		Vertex2D(glm::vec2 position, glm::vec2 texCoord)
+			: Position(position)
+			, TexCoord(texCoord)
+		{
+
+		}
+
+		Vertex2D(glm::vec2 position, glm::vec4 color)
+			: Position(position)
+			, Color(color)
+		{
+
+		}
+
+		Vertex2D(glm::vec2 position, glm::vec2 texCoord, glm::vec4 color)
+			: Position(position)
+			, TexCoord(texCoord)
+			, Color(color)
+		{
+
+		}
+	};
+}
+
+namespace hv {
+	struct Vertex3D
+	{
+		glm::vec3 Position = glm::vec3(0.0f);
+		glm::vec2 TexCoord = glm::vec2(0.0f);
+		glm::vec4 Color    = glm::vec4(1.0f);
+	
+		Vertex3D(glm::vec3 position)
+			: Position(position)
+		{
+	
+		}
+	
+		Vertex3D(glm::vec3 position, glm::vec2 texCoord)
+			: Position(position)
+			, TexCoord(texCoord)
+		{
+	
+		}
+	
+		Vertex3D(glm::vec3 position, glm::vec4 color)
+			: Position(position)
+			, Color(color)
+		{
+	
+		}
+	
+		Vertex3D(glm::vec3 position, glm::vec2 texCoord, glm::vec4 color)
+			: Position(position)
+			, TexCoord(texCoord)
+			, Color(color)
+		{
+	
+		}
 	};
 }
