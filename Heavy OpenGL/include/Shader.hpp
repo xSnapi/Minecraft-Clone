c15f56486b7f763bdfa23b6f3264570ef525c62f
xSnapi <63411420+xSnapi@users.noreply.github.com>
2022-01-31 03:57:47 +0100

    Added uniforms

--- Heavy OpenGL/include/Shader.hpp
+++ Heavy OpenGL/include/Shader.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
+
+#include "GLM/glm.hpp"
 
 namespace hv {
 	class Shader {
@@ -12,16 +15,31 @@ namespace hv {
 		void LoadFromFile(const std::string& vertex, const std::string& fragment);
 		void LoadFromMemory(const std::string& vertex, const std::string& fragment);
 
-		void Bind();
-		void Unbind();
+		void Bind() const;
+		void Unbind() const;
+
+		// Uniforms
+		void SetUniformFloat(const std::string& name, float value);
+
+		void SetUniformVec2(const std::string& name, glm::vec2 vec);
+
+		void SetUniformVec3(const std::string& name, glm::vec3 vec);
+
+		void SetUniformVec4(const std::string& name, glm::vec4 vec);
+
+		void SetUniformMat4(const std::string& name, const glm::mat4& mat, uint32_t count = 1);
 
 	private:
 		uint32_t m_id = 0;
+
+		std::unordered_map<std::string, int> m_uniformCache;
 
 		std::string Load(const std::string& path);
 
 		void Create(const std::string& vertex, const std::string& fragment);
 
 		uint32_t CompileShader(uint32_t type, const std::string& source);
+
+		int GetUniformLocation(const std::string& name);
 	};
 }
