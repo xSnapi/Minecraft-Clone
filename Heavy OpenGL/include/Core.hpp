02673850c18634dc7410a983c7f3342b62623679
xSnapi <63411420+xSnapi@users.noreply.github.com>
2022-01-31 11:42:27 +0100

    Added debug only macro

--- Heavy OpenGL/include/Core.hpp
+++ Heavy OpenGL/include/Core.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
-#define HV_DEBUG 1
+#define HV_DEBUG_ON 1
 
-#if HV_DEBUG
+#if HV_DEBUG_ON
 	#include <iostream>
 
 	#define HV_DEBUG_ASSERT(VAR) if(!(VAR))	__debugbreak();
 	#define HV_LOG(...)						DEBUG_MESSAGE_LOG(__VA_ARGS__)
+	#define HV_DEBUG(VAR)					VAR	
 
 	template<typename ...Args>
 	constexpr static void DEBUG_MESSAGE_LOG(Args&&... args)
@@ -16,4 +17,5 @@
 #else
 	#define HV_DEBUG_ASSERT(VAR)
 	#define HV_LOG(...)
+	#define HV_DEBUG(VAR)
 #endif
